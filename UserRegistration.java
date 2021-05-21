466213b184381b5cc7bb1348da2c71eeb2e434cf
SameerSS19 <sk3090019@gmail.com>
2021-05-21 22:28:41 +0530

    Enter the valid First name

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,9 +1,31 @@
 package com.registrationproblem;
+import java.util.Scanner;
+import java.util.regex.*;
 
-public class UserRegistration {
-	public static void main(String[] args) {
-		System.out.println("Welcome to User Registration");
+public class UserRegistration 
+{
+	public static boolean validname(String name) 
+	{
+		String regex = "^[A-Z]{1}[a-z A-Z]{2,}$";
+		Pattern p = Pattern.compile(regex);
+		if (name == null) 
+		{
+			return false;
+		}
+		 Matcher m = p.matcher(name);
+		 return m.matches(); 
 	}
 	
-
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to User Registration");
+		Scanner input = new Scanner(System.in);
+		System.out.print("Plase enter the your username: ");
+		String userName = input.nextLine();
+			 
+	        System.out.println(validname(userName));
+		
+	}
+	
 }
+
